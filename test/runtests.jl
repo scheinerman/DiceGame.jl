5dc14583353c796110317c5b57f0b7b50c7881aa
Ed Scheinerman <ers@jhu.edu>
2022-08-13 16:09:21 -0400

    No comment

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
 using Test, DiceGame
 
 @testset "Nontransitive Dice" begin
-    A = [2, 3, 4, 15, 16, 17]
-    B = [5, 6, 7, 8, 9, 18]
-    C = [1, 10, 11, 12, 13, 14]
+    A = [2, 3, 4, 15, 16, 17];
+    B = [5, 6, 7, 8, 9, 18];
+    C = [1, 10, 11, 12, 13, 14];
 
     @test play_dice(A, B) < 1 // 2
     @test play_dice(B, C) < 1 // 2
