ed684a1f91520aacd1f5a446c4d969a7de6745fc
Ed Scheinerman <ers@jhu.edu>
2022-08-13 15:57:22 -0400

    Working

--- src/DiceGame.jl
+++ src/DiceGame.jl
@@ -1,5 +1,51 @@
 module DiceGame
 
-greet() = print("Hello World!")
+export play_dice
+
+Die = Vector{Int}
+
+
+
+function play_dice(a::Die, b::Die)
+    a_wins = 0
+    b_wins = 0
+
+    for x ∈ a
+        for y ∈ b
+            if x > y
+                a_wins += 1
+            elseif x < y
+                b_wins += 1
+            end
+        end
+    end
+
+    total = a_wins + b_wins
+    return a_wins // total
+end
+
+function play_dice(a::Die, b::Die, c::Die)
+    a_wins = 0
+    b_wins = 0
+    c_wins = 0
+
+    for x ∈ a
+        for y ∈ b
+            for z ∈ c 
+                if x > max(y,z)
+                    a_wins += 1 
+                elseif y > max(x,z)
+                    b_wins += 1 
+                elseif z > max(x,y)
+                    c_wins += 1
+                end
+            end
+        end
+    end
+
+    total = a_wins + b_wins + c_wins
+    return (a_wins, b_wins, c_wins) .// total
+
+end
 
 end # module
